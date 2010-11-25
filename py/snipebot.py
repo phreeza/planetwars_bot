d2c157c7e7f72d2c808b08b3d5e6bd7423885b1a
Thomas McColgan <tom@Thomas-McColgans-MacBook.local>
2010-11-25 14:36:22 +0100

    typos

--- py/snipebot.py
+++ py/snipebot.py
@@ -17,7 +17,7 @@ class SnipeBot(BaseBot):
     def do_turn(self):
         self.turn += 1
         if self.turn == 1:
-            self.do_greedy:
+            self.do_greedy()
             return
         future = Future(self.universe,self.turn)
         for l in future.owner:
@@ -34,7 +34,7 @@ class SnipeBot(BaseBot):
                 if(future.owner[199][e.id] == 2 and future.owner[reach_turn][e.id] == 2 and future.ship_count[reach_turn][e.id]<p.ship_count-5-sent[p]):
                     p.send_fleet(e,future.ship_count[reach_turn][e.id]+5)
                     sent[p] += future.ship_count[reach_turn][e.id]+5
-    def do_greedy:
+    def do_greedy():
         for p in self.universe.my_planets:
             min_dist = 9999
             min_planet = None
